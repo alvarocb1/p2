6d64d559d26fa5caae6341d8fea569f4841bbaf8
Alvaro <802.291@alunos.unigran.br>
2022-11-19 16:01:29 -0400

    criacao da primeira e segunda tela

---
+++ app/src/main/java/br/unigran/p2/Activity/SegundaActivity.java
@@ -0,0 +1,16 @@
+package br.unigran.p2.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.unigran.p2.R;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
